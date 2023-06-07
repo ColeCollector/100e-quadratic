a2c7e68007e8631aa8a61b91a9da38c0e3939ec5
ColeCollector <colen827@deltalearns.ca>
2023-06-07 11:02:28 -0700

    a

--- x06_factoredquadratic.py
+++ x06_factoredquadratic.py
@@ -21,8 +21,8 @@ def Factored(a,b,c):
   None if the quadratic can not be factored
   '''
   x = x05_roots.roots(a,b,c)
-  print(x)
   if x == None:
+    print("None Found")
     return None
 
   else:
@@ -31,20 +31,50 @@ def Factored(a,b,c):
     else:
       m = x[0]
     
-    n = int(x[1]) if x[1]%1==0 else x[1]
+    if x[1] %1 == 0:
+      n = int(x[1]) 
+    else:
+      n = x[1]
 
     if m >= 0:
-      term1 = f"(x - {m})"
+      if m % 1 != 0:
+        for i in range (1,5):
+          if a%i == 0 and m*a%i == 0:
+            term1 = f"({int(a/i)}x + {int(m*a/i)})"
+
+      else:
+        term1 = f"(x - {int(m)})"
+
     else: 
       m = m*-1
-      term1 = f"(x + {m})"
+      if a != 1:
+        for i in range (1,5):
+          if a%i == 0 and m*a%i == 0:
+            term1 = f"({int(a/i)}x + {int(m*a/i)})"
+
+      else: 
+        term1 = f"(x + {int(m)})"
     
     if n >= 0:
-      term2 = f"(x - {n})"
+      if n % 1 != 0:
+        for i in range (1,5):
+          if a%i == 0 and n*a%i == 0:
+            term2 = f"({int(a/i)}x + {int(n*a/i)})"
+
+      else:
+        term1 = f"(x - {int(n)})"
+
     else: 
       n = n*-1
-      term2 = f"(x + {n})"
+      if n % 1 != 0: 
+        for i in range (1,5):
+          if a%i == 0 and n*a%i == 0:
+            term2 = f"({int(a/i)}x + {int(n*a/i)})"
+
+      else:
+        term2 = f"(x + {int(n)})"
     
+
     answer = [term1,term2]
     print(answer)
     return answer
@@ -55,8 +85,8 @@ def main():
   assert ("(x - 2)" in Factored(1,1,-6)) == True
   assert ("(x + 2)" in Factored(1,7,10)) == True
   assert ("(2x + 1)" in Factored(2,5,2)) == True
-  assert "(x + 2)" in Factored(2,5,2) == True
-  assert "(3x + 1)" in Factored(6,-7,-3) == True
+  assert ("(x + 2)" in Factored(2,5,2)) == True
+  assert ("(3x + 1)" in Factored(6,-7,-3)) == True
   assert Factored(1,4,7) == None
   assert Factored(2,4,4) == None
   
